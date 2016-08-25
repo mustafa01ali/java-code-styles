91a32d9542922f0fcca82f8ac38c75c9283f1242
Mustafa Ali <mustafa.ali@mutualmobile.com>
2016-08-25 19:36:43 +0530

    Added own codestyle

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Installs Square's IntelliJ configs into your user configs.
+# Installs Square's and other IntelliJ configs into your user configs.
 
-echo "Installing Square IntelliJ configs..."
+echo "Installing IntelliJ configs..."
 
 CONFIGS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/configs"
 
